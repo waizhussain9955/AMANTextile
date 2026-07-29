5c0fe09dee97f98e3056ce785147689efcead8f5
waizhussain9955 <waizhussain9955@gmail.com>
2026-07-29 23:48:20 +0500

    style: reduce footer animation delay to 1s

--- fix_footer_anim.js
+++ fix_footer_anim.js
@@ -6,20 +6,20 @@ files.forEach(f => {
   let content = fs.readFileSync(f, 'utf8');
   let before = content;
 
-  // Replace 2500 + to 1500 +
+  // Replace 1500 + to 1000 +
   content = content.replace(
-    /2500 \+ \(index \* 200\)/g,
-    '1500 + (index * 200)'
+    /1500 \+ \(index \* 200\)/g,
+    '1000 + (index * 200)'
   );
   
   // Replace the comment if necessary
   content = content.replace(
-    /2\.5 seconds base delay \(2500ms\)/g,
-    '1.5 seconds base delay (1500ms)'
+    /1\.5 seconds base delay \(1500ms\)/g,
+    '1 second base delay (1000ms)'
   );
 
   if (content !== before) {
     fs.writeFileSync(f, content, 'utf8');
-    console.log('Reduced animation delay in ' + f);
+    console.log('Reduced animation delay to 1s in ' + f);
   }
 });
